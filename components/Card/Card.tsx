9284eb3f7d3f97fc03a25ab959d595222d3ae793
AnthonyQY <contact@lolitek.com>
2022-09-11 11:51:54 -0700

    Challenge complete

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -1,9 +1,33 @@
+import ImageAvatar from "./image-victor.jpg";
 import styles from "./Card.module.scss";
 
 export default function Card() {
   return (
     <div className={styles.container}>
-      <div>Placeholder</div>
+      <div className={styles.content}>
+        <img className={styles.avatar} src={ImageAvatar.src} />
+        <div className={styles.info}>
+          <h1 className={styles.name}>
+            Victor Crest <span className={styles.age}>26</span>
+          </h1>
+          <h2 className={styles.location}>London</h2>
+        </div>
+      </div>
+      <hr className={styles.divider} />
+      <div className={styles.stats}>
+        <div className={styles.statistic}>
+          <h1 className={styles.statistic__count}>80K</h1>
+          <h2 className={styles.statistic__label}>Followers</h2>
+        </div>
+        <div className={styles.statistic}>
+          <h1 className={styles.statistic__count}>803K</h1>
+          <h2 className={styles.statistic__label}>Likes</h2>
+        </div>
+        <div className={styles.statistic}>
+          <h1 className={styles.statistic__count}>1.4K</h1>
+          <h2 className={styles.statistic__label}>Photos</h2>
+        </div>
+      </div>
     </div>
   );
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -25,7 +25,7 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Outfit:wght@400;700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Kumbh+Sans:wght@400;700&display=swap"
           rel="stylesheet"
         />
       </Head>
